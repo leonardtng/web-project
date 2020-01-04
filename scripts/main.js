f48cdc4b0c2590f8799b012b35e49b94ee5ee4cd
Leonard Tng <leonardtng@gmail.com>
2020-01-04 23:59:05 +0800

    Added a topbar

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,24 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/boy.png');
     }
 }
+
+let myButton = document.querySelector('.change-username');
+let logInInfo = document.querySelector('.log-in-info');
+logInInfo.textContent = 'You are not logged in'
+
+function setUsername() {
+  let myName = prompt('Please enter your name: ');
+  localStorage.setItem('Name', myName);
+  logInInfo.textContent = myName + ' is logged in';
+}
+
+if (!localStorage.getItem('Name')) {
+  setUsername();
+}else{
+  let storedName = localStorage.getItem('Name');
+  logInInfo.textContent = storedName + ' is logged in';
+}
+
+myButton.onclick = function() {
+  setUsername();
+}
